f3eacf54a34ecf1b2cfb9e92ac329f2a3dab77bd
nabeisham <144115932+nabeisham@users.noreply.github.com>
2024-07-17 11:44:11 +0530

    home-page-designing

--- Front-end/src/components/MindfulnessTourismSection.jsx
+++ Front-end/src/components/MindfulnessTourismSection.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './CSS/MindfulnessTourismSection.css';
+import img from '../assets/images/mindfulness_tourism_bg.png'
 
 const MindfulnessTourismSection = () => {
 
@@ -21,18 +22,15 @@ const MindfulnessTourismSection = () => {
 
   return (
     <section id="mindfulness-tourism-section" className="mindfulness-tourism-section">
-      <div className="mindfulness-tourism-container">
-        <div className="mindfulness-tourism-content" style={{ textAlign: 'left', padding: '0% 10% 0% 50%' }}>
-              <h6 className='font-secondary' style={{ marginTop: '-6rem'}}>Explore</h6>
-              <hr style={{ width: '3.5rem' }} />
-              <h3 className='font-primary'>Mindfulness Tourism</h3>
-              <p className='font-secondary'>Embraced by verdant jungle with panoramic views of the Indian Ocean, ÀNI Sri Lanka, an all-inclusive luxury private resort, welcomes exclusively your family and friends alone to this private beachfront estate on the island’s fabled south coast.</p>
-              <br></br>
-              <p>
-                <button type="button" className="btn btn-outline-light" style={{}}>Button</button>
-                <button type="button" className="btn btn-link" style={{ textDecoration: 'none', color: 'white' }}>Learn more ></button>
-              </p>
-        </div>
+      <div className="overlayert"></div>
+      <img src={img} className="mindfulness-img" alt="" />
+      <div className="mindfulness-tourism-content">
+            <h6 className='font-secondary' style={{ marginTop: '-6rem'}}>Explore</h6>
+            <hr style={{ width: '3.5rem' }} />
+            <h3 className='font-primary'>Mindfulness Tourism</h3>
+            <p className='font-secondary'>Embraced by verdant jungle with panoramic views of the Indian Ocean, ÀNI Sri Lanka, an all-inclusive luxury private resort, welcomes exclusively your family and friends alone to this private beachfront estate on the island’s fabled south coast.</p>
+            <br></br>
+            <button type="button" className="btn btn-outline-light">Learn more ></button>
       </div>
     </section>
   );
