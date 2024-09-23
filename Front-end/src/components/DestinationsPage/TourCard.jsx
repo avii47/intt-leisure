cee6188e7327b648ad6ba21406f3bb0923f6bb30
nabeisham <142118907+avii47@users.noreply.github.com>
2024-09-23 13:10:37 +0530

    updated

--- Front-end/src/components/DestinationsPage/TourCard.jsx
+++ Front-end/src/components/DestinationsPage/TourCard.jsx
@@ -4,7 +4,7 @@ import '../CSS/Pages/InnerTourCard.css';
 const TourComponent = ({ content, onInquiryClick, onCallClick  }) => {
   return (
     <div className="tour-component col-12">
-      <div className="tour-left col-md-3">
+      <div className="tour-left col-lg-3">
         <img 
           src={content.img} 
           alt={content.title} 
@@ -12,7 +12,7 @@ const TourComponent = ({ content, onInquiryClick, onCallClick  }) => {
         />
       </div>
 
-      <div className="tour-middle col-md-6">
+      <div className="tour-middle col-lg">
         <h1>{content.title}</h1>
         <ul className="tour-details">
           {content.details.map((detail, index) => (
@@ -21,14 +21,15 @@ const TourComponent = ({ content, onInquiryClick, onCallClick  }) => {
         </ul>
       </div>
 
-      <div className="col-md-3 tour-right">
-        <div className="tour-starting-price">
-          Starting price <br /> <span>{content.startingPrice}</span>
-        </div><hr />
-        <div className="tour-buttons">
-          <button className="inquiry-button" onClick={onInquiryClick}>Submit Inquiry</button><br />
-          <button className="call-button" onClick={onCallClick}>Call now</button>
-        </div>
+      <div className="col-lg-3 tour-right">
+          <div className="tour-starting-price">
+            Starting price <br /> <span>{content.startingPrice}</span>
+            <hr />
+          </div>
+          <div className="tour-buttons">
+            <button className="inquiry-button" onClick={onInquiryClick}>Submit Inquiry</button>
+            <button className="call-button" onClick={onCallClick}>Call now</button>
+          </div>
       </div>
     </div>
   );
--- Front-end/src/components/Events&NewsPage/Events&NewsContent.jsx
+++ Front-end/src/components/Events&NewsPage/Events&NewsContent.jsx
@@ -1,26 +1,22 @@
-import React, { useEffect, lazy } from 'react';
-import { useMobileView } from '../../contexts/MobileViewContext';
+import React, { useEffect, lazy } from "react";
+import { useMobileView } from "../../contexts/MobileViewContext";
 import eventstData from "../../data/EventstData";
 import newsData from "../../data/NewsData";
 
-
-
-const EventCard = lazy(() => import('../Events&NewsPage/EventCard'));
-const NewsCard = lazy(() => import('../Events&NewsPage/NewsCard'));
+const EventCard = lazy(() => import("../Events&NewsPage/EventCard"));
+const NewsCard = lazy(() => import("../Events&NewsPage/NewsCard"));
 
 const EventsNewsContent = () => {
-
   const isMobileView = useMobileView();
-  
 
   useEffect(() => {
-    const section = document.getElementById('rw1');
-    const textSection = document.getElementById('text1');
+    const section = document.getElementById("rw1");
+    const textSection = document.getElementById("text1");
 
     const observer = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
-          textSection.classList.add('animate-text');
+          textSection.classList.add("animate-text");
         }
       },
       { threshold: 0.5 }
@@ -38,13 +34,13 @@ const EventsNewsContent = () => {
   }, []);
 
   useEffect(() => {
-    const section = document.getElementById('rw2');
-    const textSection = document.getElementById('text2');
+    const section = document.getElementById("rw2");
+    const textSection = document.getElementById("text2");
 
     const observer = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
-          textSection.classList.add('animate-text');
+          textSection.classList.add("animate-text");
         }
       },
       { threshold: 0.5 }
@@ -62,13 +58,13 @@ const EventsNewsContent = () => {
   }, []);
 
   useEffect(() => {
-    const section = document.getElementById('rw3');
-    const textSection = document.getElementById('text3');
+    const section = document.getElementById("rw3");
+    const textSection = document.getElementById("text3");
 
     const observer = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
-          textSection.classList.add('animate-text');
+          textSection.classList.add("animate-text");
         }
       },
       { threshold: 0.5 }
@@ -84,25 +80,31 @@ const EventsNewsContent = () => {
       }
     };
   }, []);
-
-
-
-
 
   return (
-    <section id="eventsNews-content-section" className={`section eventsNews-content-section justify-content-center d-flex ${isMobileView ? 'mobile-view' : ''}`}>
+    <section
+      id="eventsNews-content-section"
+      className={`section eventsNews-content-section justify-content-center d-flex ${
+        isMobileView ? "mobile-view" : ""
+      }`}
+    >
       <div className="eventsNews-section-content justify-content-center">
-      <h3 className='font-primary' style={{marginBottom:'50px'}}><b>Upcoming Events</b></h3>
+        <h3 className="font-primary" style={{ marginBottom: "50px" }}>
+          <b>Events</b>
+        </h3>
         <div className="events-cards-container">
-            {eventstData.map((content, index) => (
-              <EventCard key={index} content={content} />
-            ))}
-        </div><br></br>
-        <h3 className='font-primary' style={{marginBottom:'50px'}}><b>Latest Events & News</b></h3>
+          {eventstData.map((content, index) => (
+            <EventCard key={index} content={content} />
+          ))}
+        </div>
+        <br></br>
+        <h3 className="font-primary" style={{ marginBottom: "50px" }}>
+          <b>Latest Events & News</b>
+        </h3>
         <div className="news-cards-container d-flex">
-            {newsData.map((content, index) => (
-              <NewsCard key={index} content={content} />
-            ))}
+          {newsData.map((content, index) => (
+            <NewsCard key={index} content={content} />
+          ))}
         </div>
       </div>
     </section>
