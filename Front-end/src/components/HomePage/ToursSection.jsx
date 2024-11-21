4fd57c8ffcbfb422df9c9ba4588120b7ae6157e3
Ashan Avishka <142118907+avii47@users.noreply.github.com>
2024-11-21 10:11:43 +0530

    up

--- Front-end/src/components/HomePage/ToursSection.jsx
+++ Front-end/src/components/HomePage/ToursSection.jsx
@@ -23,19 +23,16 @@ function ToursSection({ imageUrl, title, sub, description, spec1, spec1Icon, spe
     return (
         <div className={`tourSection-container  ${isMobileView ? 'mobile-view' : ''}`} style={{ marginTop: isMobileView ? '4rem' : '8rem' }}>
             <div className="tourSection-content">
-                {(vertical) && (<div className="col-md-6 d-flex" style={{ justifyContent: 'center' }}>
-                    <img src={imageUrl} alt="Tour" className="tourSection-card-image-v" />
-                </div>)}
-                {(!vertical) && (<div className="col-md-6 d-flex" style={{ justifyContent: 'left' }}>
-                    <img src={imageUrl} alt="Tour" className="tourSection-card-image" />
-                </div>)}
-                {/* <div className="col-md-6 d-flex" style={{ justifyContent: 'center' }}>
-                    <img src={imageUrl} alt="Tour" className="tourSection-card-image" />
-                </div> */}
-                <div className="col-md-6 tourSection-card-con">
-                    <div className="tourSection-card-content">
+                {(isMobileView) && (
+                    <>
                         <h5 className="card-subtitle font-secondary">{sub}</h5><hr style={{ width: isMobileView ? '65%' : '32%' }} />
                         <h1 className="font-primary">{title}</h1>
+                        {(vertical) && (<div className="col-md-6 d-flex" style={{ justifyContent: 'center' }}>
+                            <img src={imageUrl} alt="Tour" className="tourSection-card-image-v" />
+                        </div>)}
+                        {(!vertical) && (<div className="col-md-6 d-flex" style={{ justifyContent: 'left' }}>
+                            <img src={imageUrl} alt="Tour" className="tourSection-card-image" />
+                        </div>)}
                         <p className="card-description font-secondary">{description}</p>
                         <div className="card-options">
                             <div className="card-option">
@@ -48,8 +45,39 @@ function ToursSection({ imageUrl, title, sub, description, spec1, spec1Icon, spe
                             </div>
                         </div>
                         <button className="card-button" onClick={() => handleOnClick(`${link}`)}>{buttonLabel}</button>
-                    </div>
-                </div>
+                    </>
+                )}
+
+                {(!isMobileView) && (
+                    <>
+                        {(vertical) && (<div className="col-md-6 d-flex" style={{ justifyContent: 'center' }}>
+                            <img src={imageUrl} alt="Tour" className="tourSection-card-image-v" />
+                        </div>)}
+                        {(!vertical) && (<div className="col-md-6 d-flex" style={{ justifyContent: 'left' }}>
+                            <img src={imageUrl} alt="Tour" className="tourSection-card-image" />
+                        </div>)}
+                        <div className="col-md-6 tourSection-card-con">
+                            <div className="tourSection-card-content">
+                                <h5 className="card-subtitle font-secondary">{sub}</h5><hr style={{ width: isMobileView ? '65%' : '32%' }} />
+                                <h1 className="font-primary">{title}</h1>
+                                <p className="card-description font-secondary">{description}</p>
+                                <div className="card-options">
+                                    <div className="card-option">
+                                        <img src={spec1Icon} className="card-icon" />
+                                        <span className='font-secondary' style={{ fontSize: isMobileView ? '12px' : '' }}>{spec1}</span>
+                                    </div>
+                                    <div className="card-option">
+                                        <img src={spec2Icon} className="card-icon" />
+                                        <span className='font-secondary' style={{ fontSize: isMobileView ? '12px' : '' }}>{spec2}</span>
+                                    </div>
+                                </div>
+                                <button className="card-button" onClick={() => handleOnClick(`${link}`)}>{buttonLabel}</button>
+                            </div>
+                        </div>
+                    </>
+                )}
+
+
 
 
             </div>
